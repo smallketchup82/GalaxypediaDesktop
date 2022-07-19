4598ec70e9f50da03ff45207bbcee57ff367820a
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2022-07-18 23:41:12 -0400

    fix indentation

--- build-linux.js
+++ build-linux.js
@@ -2,26 +2,26 @@ var nativefier = require('nativefier').default;
 const chalk = require('chalk');
 
 var options = {
-    name: 'Galaxypedia',
-    targetUrl: 'https://robloxgalaxy.wiki',
-    platform: 'linux',
-    arch: 'x64',
-    out: '.',
-    internalUrls: 'http.:\/\/robloxgalaxy\.wiki',
-    strictInternalUrls: true,
-    showMenuBar: true
-  };
+	name: 'Galaxypedia',
+	targetUrl: 'https://robloxgalaxy.wiki',
+	platform: 'linux',
+	arch: 'x64',
+	out: '.',
+	internalUrls: 'http.:\/\/robloxgalaxy\.wiki',
+	strictInternalUrls: true,
+	showMenuBar: true
+};
 
 console.log(chalk.redBright(`Linux support is unstable and may have errors. Use at your own risk\nTo cancel, terminate the script within the next 5 seconds`))
 
 setTimeout(() => {
-  console.log("Building Galaxypedia Desktop App for Linux...")
+	console.log("Building Galaxypedia Desktop App for Linux...")
 
-  nativefier(options, function (error, appPath) {
-    if (error) {
-      throw error.message
-    }
-    console.log('GalaxypediaDesktop Linux has been nativefied to', appPath);
-  });
+	nativefier(options, function (error, appPath) {
+		if (error) {
+			throw error.message
+		}
+		console.log('GalaxypediaDesktop Linux has been nativefied to', appPath);
+	});
 
 }, 5000);
